c5d314eff1d11c0c691c52dc351e54ebc9026ffe
Sampo Ylisiurua <110446843+Skullervo@users.noreply.github.com>
2024-09-06 14:53:29 +0300

    Delete Mac v.1.0 directory

--- Mac v.1.0/Functions/determineReconstruction.m
+++
@@ -1,7 +0,0 @@
-function reco=determineReconstruction(reconstruction,i,ParametersinfoListBox,BeamgeometryButton)
-            reco=reconstruction;
-            t = datetime('Now')
-            t=datestr(t)
-            ParametersinfoListBox.Items(i) = {strcat(t, ' <Reconstruction algorithm: ',reco,'>')}
-            BeamgeometryButton.Enable ='on'
-end
